0485e29c723cc5c8ece0f849cdabe0b3b0956015
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-07-31 10:36:11 +0800

    chore: base setting + config

---
+++ scraper/config.py
@@ -0,0 +1,32 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+
+    login_proxy: str
+    password_proxy: str
+    host_proxy: str
+    port_proxy: str
+
+    url_host: str
+    chromedriver: str
+
+    login_emex: str
+    password_emex: str
+
+    api_order: str
+
+
+config = Config(
+    login_proxy=os.environ['LOGIN_PROXY'],
+    password_proxy=os.environ['PASSWORD_PROXY'],
+    host_proxy=os.environ['HOST_PROXY'],
+    port_proxy=os.environ['PORT_PROXY'],
+    url_host=os.environ['URL_HOST'],
+    chromedriver=os.environ['CHROMEDRIVER'],
+    login_emex=os.environ['LOGIN_EMEX'],
+    password_emex=os.environ['PASSWORD_EMEX'],
+    api_order=os.environ['API_ORDER']
+)
