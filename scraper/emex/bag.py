e5fb62dd003953a67e2430707b96032e5e817690
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-08-06 17:54:33 +0800

    feature: add model emex

--- scraper/emex/bag.py
+++ scraper/emex/bag.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from seleniumwire import webdriver
 from seleniumwire.request import Request
 
@@ -6,16 +8,16 @@ from scraper.config import config
 
 class Bag:
 
-    def __init__(self, webdriver):
+    def __init__(self, webdriver: webdriver):
         self.webdriver = webdriver
 
-    def _to_bytes(self, body: dict) -> bytes:
+    def _to_bytes(self, body: dict[str, str]) -> str:
         body_list = []
         for key, value in body.items():
             body_list.append(f'{key}={value}')
         return '&'.join(body_list)
 
-    def _to_dict(self, request: Request) -> dict:
+    def _to_dict(self, request: Request) -> dict[str, str]:
         body = request.body.decode('utf-8').split('&')
         req = {}
         for i in body:
@@ -27,18 +29,18 @@ class Bag:
         req['countOnPage'] = 100
         return req
 
-    def edit_request(self, body: Request, params: dict):
+    def edit_request(self, body: Request, params: dict[Any, Any]):
         request = self._to_dict(body)
         for key, value in params.items():
             request[key] = value
         return self._to_bytes(request)
 
-    def cookies(self, driver: webdriver) -> dict:
+    def cookies(self, driver: webdriver) -> dict[str, str]:
         cookies = driver.get_cookies()
         cookie = {c['name']: c['value'] for c in cookies}
         return cookie
 
-    def headers(self, response: Request) -> dict:
+    def headers(self, response: Request) -> dict[str, str]:
         headers = {}
         payload = response.headers._headers
         for header in payload:
